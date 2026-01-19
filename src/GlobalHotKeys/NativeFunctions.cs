2e28aaefbbb30c14067d34636461f991723432b7
romeoahmed <ahmedorqwn@gmail.com>
2026-01-19 22:46:25 +0800

    refactor: restrict visibility of NativeFunctions and P/Invoke methods to internal

--- src/GlobalHotKeys/NativeFunctions.cs
+++ src/GlobalHotKeys/NativeFunctions.cs
@@ -2,50 +2,50 @@ using System.Runtime.InteropServices;
 
 namespace GlobalHotKeys;
 
-public partial class NativeFunctions
+internal static partial class NativeFunctions
 {
     [LibraryImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static partial bool RegisterHotKey(IntPtr hWnd, int id, Modifiers fsModifiers, VirtualKeyCode vk);
+    internal static partial bool RegisterHotKey(IntPtr hWnd, int id, Modifiers fsModifiers, VirtualKeyCode vk);
 
     [LibraryImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static partial bool UnregisterHotKey(IntPtr hWnd, int id);
+    internal static partial bool UnregisterHotKey(IntPtr hWnd, int id);
 
     [LibraryImport("user32.dll", SetLastError = true)]
-    public static partial IntPtr DefWindowProc(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
+    internal static partial IntPtr DefWindowProc(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
 
     [LibraryImport("user32.dll", SetLastError = true)]
-    public static partial int RegisterClassEx(ref WNDCLASSEX lpwcx);
+    internal static partial int RegisterClassEx(ref WNDCLASSEX lpwcx);
 
     [LibraryImport("user32.dll", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
-    public static partial IntPtr CreateWindowEx(int dwExStyle, uint regResult, string? lpWindowName, WindowStyle dwStyle, int x, int y, int nWidth, int nHeight, IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lpParam);
+    internal static partial IntPtr CreateWindowEx(int dwExStyle, uint regResult, string? lpWindowName, WindowStyle dwStyle, int x, int y, int nWidth, int nHeight, IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lpParam);
 
     [LibraryImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static partial bool DestroyWindow(IntPtr hWnd);
+    internal static partial bool DestroyWindow(IntPtr hWnd);
 
     [LibraryImport("user32.dll", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static partial bool UnregisterClass(string lpClassName, IntPtr hInstance);
+    internal static partial bool UnregisterClass(string lpClassName, IntPtr hInstance);
 
     [LibraryImport("user32.dll")]
-    public static partial int GetMessage(ref TagMSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
+    internal static partial int GetMessage(ref TagMSG lpMsg, IntPtr hWnd, uint wMsgFilterMin, uint wMsgFilterMax);
 
     [LibraryImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static partial bool TranslateMessage(ref TagMSG lpMsg);
+    internal static partial bool TranslateMessage(ref TagMSG lpMsg);
 
     [LibraryImport("user32.dll")]
-    public static partial IntPtr DispatchMessage(ref TagMSG lpmsg);
+    internal static partial IntPtr DispatchMessage(ref TagMSG lpmsg);
 
     [LibraryImport("kernel32.dll", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
-    public static partial IntPtr GetModuleHandle(string? lpModuleName);
+    internal static partial IntPtr GetModuleHandle(string? lpModuleName);
 
     [LibraryImport("user32.dll")]
-    public static partial IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+    internal static partial IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
     [LibraryImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
-    public static partial bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+    internal static partial bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 }
